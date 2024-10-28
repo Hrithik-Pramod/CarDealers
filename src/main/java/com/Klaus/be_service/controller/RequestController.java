c3dffbd9ebc7a43cad2766f077f9c815540ea286
hrithik-at-105410518218 <hrithik.pramod@elogosoft.in>
2024-10-28 22:52:52 +0530

    Initial Commit

--- src/main/java/com/Klaus/be_service/controller/RequestController.java
+++ src/main/java/com/Klaus/be_service/controller/RequestController.java
@@ -22,6 +22,8 @@ public class RequestController {
     EmailService emailService;
     @Value("${email.to}")
     String to;
+    @Value("${email.benz}")
+    String tobenz;
 
     @PostMapping("/enquiry")
     public String enquiry(
@@ -64,6 +66,38 @@ public class RequestController {
         }
         buffer.flush();
         return buffer.toByteArray();
+    }
+
+    @PostMapping("/enquiry2")
+    public String enquiry2(
+            @RequestBody(required = true) Request request) throws IOException {
+        try {
+
+//            ClassPathResource imageResource = new ClassPathResource(logoPath);
+//
+//            // Read image bytes using plain Java
+//            byte[] imageBytes = readImageAsBytes(imageResource.getInputStream());
+//            String base64Logo = Base64.getEncoder().encodeToString(imageBytes);
+            Map<String, Object> templateModel = new HashMap<>();
+            templateModel.put("name", request.getName());
+            templateModel.put("phone", request.getPhoneNumber());
+            templateModel.put("email", request.getEmailId());
+            templateModel.put("model", request.getModel());
+            templateModel.put("price", request.getPrice());
+//            templateModel.put("base64Logo", base64Logo);
+
+            emailService.sendHtmlMessage(
+                    tobenz, // Admin email
+                    "New Car Inquiry",
+                    "email-template1",  // Thymeleaf template name (without .html)
+                    templateModel
+            );
+            return "Inquiry sent!\n" +
+                    "You’ll hear from us soon!";
+        } catch (MessagingException E) {
+            return "Failed to send inquiry: " + E.getLocalizedMessage();
+        }
+
     }
 
 }
